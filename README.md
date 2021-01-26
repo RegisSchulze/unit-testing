1042a2de00725cd4f1de32de99d80df8f8306534
Regis Schulze <schulze_regis@hotmail.com>
2021-01-26 15:16:01 +0100

    updated README.md file and first try on test_get_users_list_from_db

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # unit-testing
+Challenge: Unit testing
+
+Let's partice now!
+
+Repository: unit-testing
+Type of Challenge: Learning
+Duration: 1 days
+Deadline: dd/mm/yy H:i AM/PM
+Team challenge : solo
+Learning Objectives
+You will learn how to take advantage of unit testing to create robust code. At the end of the challenge, you will be able to:
+
+Test your code with the assert python keyword
+Test your code with the unittest module
+Test your code with pytest module
+Use Mock objects
+Use other features from pytest
+The Mission
+You work for a company working in Python. Your customer complains that your code crashes a lot and they're not able to access your services half of the time.
+
+You see all the seniors of your company debugging all day and being mad at their colleagues breaking the code everytime they want to add a feature.
+
+Your project manager asks you to write unit tests to fix the problem.
+
+The code can be found under the unit-testing.py
--- unit-testing.py
+++ unit-testing.py
@@ -29,10 +29,12 @@ def connect_to_db(connection_string: str):
     db = mock.MagicMock()  # mock db connection
 
     print("connection string: ", connection_string)
-    if connection_string == "test":
+    if connection_string == "mock":
+        pass
+    elif connection_string == "test":
         raise TestDbError("ERROR: YOU FORGOT TO MOCK connect_to_db")
     else:
-        raise ConnectionDatabaseError("Can't connect to the databse!")
+        raise ConnectionDatabaseError("Can't connect to the database!")
 
 
 def get_users_list_from_db(connection_string: str) -> List[Dict[str, str]]:
@@ -43,7 +45,26 @@ def get_users_list_from_db(connection_string: str) -> List[Dict[str, str]]:
     The unit test should check that all the users have a username, a birthday and a role.
     """
     db = connect_to_db(connection_string)
-    users = db.get_user()
+    users = [{'username': 'John Doe', 'birthday': '02/12/1985', 'role': 'admin'},
+             {'username': 'Frank Sinatra', 'birthday': '04/09/1935', 'role': 'singer'},
+             {'username': 'Regis Schulze', 'birthday': '31/12/1990', 'role': 'junior becodian'},
+             {'username': 'Frederic de Reu', 'birthday': '20/06/1992', 'role': 'project manager'},
+             {'username': 'Cedric Van Rensbergen', 'birthday': '21/12/1989', 'role': 'sales'},
+             {'username': 'Eden Hazard', 'birthday': '08/06/1990', 'role': 'footballer'},
+             {'username': 'Snoop Dogg', 'birthday': '16/05/1970', 'role': 'rapper'},
+             {'username': 'Jessica Alba', 'birthday': '15/11/1972', 'role': 'actress'},
+             {'username': 'Idris Elba', 'birthday': '02/08/1965', 'role': 'actor'},
+             {'username': 'Joe Biden', 'birthday': '12/06/1945', 'role': 'president'},
+             {'username': 'ELon Musk', 'birthday': '02/04/1971', 'role': 'genius'},
+             {'username': 'Conor Mcgregor', 'birthday': '25/04/1988', 'role': 'fighter'},
+             {'username': 'Julie Schulze', 'birthday': '24/06/1988', 'role': 'sister'},
+             {'username': 'Jos Vermuilen', 'birthday': '09/08/1968', 'role': 'tennisplayer'},
+             {'username': 'Pablo Picasso', 'birthday': '02/06/1910', 'role': 'artist'},
+             {'username': 'Kendrick Lamar', 'birthday': '02/12/1986', 'role': 'rapper'},
+             {'username': '50 Cent', 'birthday': '02/11/1975', 'role': 'rapper'},
+             {'username': 'Jay Rock', 'birthday': '04/06/1981', 'role': 'rapper'},
+             {'username': 'Mos Def', 'birthday': '29/04/1973', 'role': 'rapper'},
+             {'username': 'jonh Doe II', 'birthday': '24/11/2011', 'role': 'future admin'}]
     return users
 
 
@@ -65,8 +86,9 @@ class TestMathFunctions(unittest.TestCase):
             connect_to_db("testing")
 
     def test_get_users_list_from_db(self):
-
-        pass
+        users = get_users_list_from_db("mock")
+        print(f'users: {len(users)}')
+        assert len(users) >= 20
 
     def test_add(self):
         """
